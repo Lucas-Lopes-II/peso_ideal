bb27bb78ee55e3ecba054370aeba85d7d9ee883b
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2022-01-02 08:34:52 -0300

    Melhorando o código

--- js/script.js
+++ js/script.js
@@ -6,55 +6,60 @@ let limpar = document.querySelector('#limpar');
 let container = document.querySelector('#divMsg');
 
 //Função de calcular os dados e dizer qual é o peso ideal
-calcular.onclick = function (){
+document.querySelector('#calcular').addEventListener('click', () => {
+
     limparMsg();
 
     if(nome.value === ''){
-        let msg = document.createTextNode('Digite um nome');
-        let pElement = document.createElement('p');
-        pElement.setAttribute('class', 'erro');
-        pElement.appendChild(msg);
-        container.appendChild(pElement);
+
+        container.innerHTML = `<p class="erro">Digite um nome<p>`;
+
     }else if(altura.value === '' || isNaN(altura.value) === true) {
-        let msg = document.createTextNode(`${nome.value}, digite sua altura`);
-        let pElement = document.createElement('p');
-        pElement.setAttribute('class', 'erro');
-        pElement.appendChild(msg);
-        container.appendChild(pElement);
+
+        container.innerHTML = `<p class="erro">${nome.value}, digite sua altura<p>`;
+        alert('Use um . (ponto) para separa os decimais');
+
     }else if(sexo.value === 'vazio') {
-        let msg = document.createTextNode(`${nome.value}, selecione seu sexo`);
-        let pElement = document.createElement('p');
-        pElement.setAttribute('class', 'erro');
-        pElement.appendChild(msg);
-        container.appendChild(pElement);
+
+        container.innerHTML = `<p class="erro">${nome.value}, selecione seu sexo<p>`;
+
     }else{
+
         let gen = 0
         sexo.value = 'm' ? gen = 22 : gen = 21;
-        let calculo = (gen * (Math.pow(altura.value, 2))).toFixed(2); 
-        let msg = document.createTextNode(`${nome.value}, seu peso ideal é ${calculo}kg`);
-        let pElement = document.createElement('p');
-        pElement.setAttribute('class', 'pesoIdeal');
-        pElement.appendChild(msg);
-        container.appendChild(pElement);
+        let calculo = (gen * (Math.pow(altura.value, 2))).toFixed(2);
+
+        container.innerHTML = `<p class="erro">${nome.value}, seu peso ideal é ${calculo}kg<p>`
+
     }
-}
+
+});
 
 //Função de limpar todos os inputs e menssagens
 limpar.onclick = function(){
+
     nome.value = '';
     altura.value = '';
     sexo.value = 'vazio';
     limparMsg();
+
 }
 
 //Limpa menssagens
 function limparMsg(){
+
     let elementoP = document.querySelectorAll('p');
+
     if(elementoP.length === 0){
+
         return;
+
     }else{
+
         container.textContent = '';
+
     }    
+
 }
 
 
@@ -64,15 +69,21 @@ let limpaSexo = document.querySelector('#excluir_sexo');
 
 //Função que limpa input do nome
 limpaNome.onclick = function() {
+
     nome.value = '';
+
 }
 
 //Função que limpa input do altura
 limpaAltura.onclick = function() {
+    
     altura.value = '';
+
 }
 
 //Função que limpa select de sexo
 limpaSexo.onclick = function() {
+
     sexo.value = 'vazio';
+
 }
