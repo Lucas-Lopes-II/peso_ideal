dc064ba87a41738d7fca60ddd8006b682bfd4aef
Lucas Lopes <92945468+Lucas-Lopes-II@users.noreply.github.com>
2022-03-06 09:08:28 -0300

    Melhorando o código

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 let nome = document.querySelector('#ipt_nome');
 let altura = document.querySelector('#ipt_altura');
 let sexo = document.querySelector('#select_altura');
-let calcular = document.querySelector('#calcular');
-let limpar = document.querySelector('#limpar');
 let container = document.querySelector('#divMsg');
 
 //Função de calcular os dados e dizer qual é o peso ideal
@@ -12,7 +10,7 @@ document.querySelector('#calcular').addEventListener('click', () => {
 
     if(nome.value === ''){
         container.innerHTML = `<p class="erro">Digite um nome<p>`;
-    }else if(altura.value === '' || isNaN(altura.value) === true) {
+    }else if(altura.value === '' || isNaN(altura.value)) {
         container.innerHTML = `<p class="erro">${nome.value}, digite sua altura<p>`;
         alert('Use um . (ponto) para separa os decimais');
     }else if(sexo.value === 'vazio') {
@@ -21,18 +19,17 @@ document.querySelector('#calcular').addEventListener('click', () => {
         let gen = 0
         sexo.value = 'm' ? gen = 22 : gen = 21;
         let calculo = (gen * (Math.pow(altura.value, 2))).toFixed(2);
-        container.innerHTML = `<p class="erro">${nome.value}, seu peso ideal é ${calculo}kg<p>`
+        container.innerHTML = `<p class="erro">${nome.value}, seu peso ideal é ${calculo}kg<p>`;
     }
 });
 
 //Função de limpar todos os inputs e menssagens
-limpar.onclick = function(){
-
+document.querySelector('#limpar').addEventListener('click', () => {
     nome.value = '';
     altura.value = '';
     sexo.value = 'vazio';
     limparMsg();
-}
+});
 
 //Limpa menssagens
 function limparMsg(){
@@ -46,22 +43,17 @@ function limparMsg(){
     }    
 }
 
-
-let limpaNome = document.querySelector('#excluir_nome');
-let limpaAltura = document.querySelector('#excluir_altura');
-let limpaSexo = document.querySelector('#excluir_sexo');
-
 //Função que limpa input do nome
-limpaNome.onclick = function() {
+document.querySelector('#excluir_nome').addEventListener('click', () => {
     nome.value = '';
-}
+});
 
 //Função que limpa input do altura
-limpaAltura.onclick = function() {   
+document.querySelector('#excluir_altura').addEventListener('click', () =>{   
     altura.value = '';
-}
+});
 
 //Função que limpa select de sexo
-limpaSexo.onclick = function() {
+let limpaSexo = document.querySelector('#excluir_sexo').addEventListener('click', () => {
     sexo.value = 'vazio';
-}
+});
